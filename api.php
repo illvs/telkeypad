b19f20afece8ae97e349097de89a349b997fd1e3
illvs <ilvs@tuta.io>
2019-01-08 11:32:36 -0600

    check whether input isn't an empty string instead of truthy

--- api.php
+++ api.php
@@ -3,4 +3,8 @@ require 'trash.php';
 
 $in = htmlspecialchars($_GET['q']);
 
-echo $in ? (preg_match('/^([0-9*#]+)$/', $in) ? $convert($in) : 'invalid char received') : 'no input received';
+if ($in !== '' && preg_match('/^([0-9*#]+)$/', $in)) {
+    echo $convert($in);
+} else {
+    echo 'invalid input';
+}
